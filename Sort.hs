d8bb2ab11cfab11c72c5d53b2eb87a616f499216
ufukguenes <u.guenes@live.de>
2022-03-19 01:47:03 +0100

    moved the sorting functions into seperate module and added some comments

--- Sort.hs
+++ Sort.hs
@@ -1,29 +1,34 @@
 module Sort where
 
+-- merge operation with adjustable comparrsion function
 merge comp [] ys = ys
 merge comp xs [] = xs
 merge comp (x:xs) (y:ys)
   | comp x y = x : merge comp xs (y:ys)
   | otherwise = y : merge comp (x:xs) ys
   
+-- merge sort
 mergeSort comp [] = []
 mergeSort comp [x] = [x]
 mergeSort comp xs = merge comp (mergeSort comp (firstN half (xs))) (mergeSort comp (fromN half xs))
   where 
     half = div (length xs) 2
     
+-- first n elements of an array
 firstN 0 xs = []
 firstN n (x:xs) = x : firstN (n-1) xs
 
+-- last n elements of an array
 fromN 0 xs = xs
 fromN n (x:xs) = fromN (n-1) xs
 
-
-arrayComparrisor [] ys = True
-arrayComparrisor xs [] = False
-arrayComparrisor (x:xs) (y:ys) 
+-- compares two arrays
+arrayComparritor [] ys = True
+arrayComparritor xs [] = False
+arrayComparritor (x:xs) (y:ys) 
     | x < y = True
     | x > y = False
-    | otherwise = arrayComparrisor xs ys
+    | otherwise = arrayComparritor xs ys
     
-tupleComparrisor (x,c) (y,i) = arrayComparrisor x y
+-- compares two tuples which have arrays as their first elements
+tupleArrayComparritor (x,c) (y,i) = arrayComparritor x y
--- SuffixArray.hs
+++ SuffixArray.hs
@@ -1,5 +1,7 @@
+module SuffixArray where
+import Sort
 
--- create one suffix from index i
+-- create one suffix starting from index i
 suffixFrom [] i = error "Index larger then string"
 suffixFrom xs 0 = xs
 suffixFrom (x:xs) i = suffixFrom xs (i - 1)
@@ -8,16 +10,16 @@ suffixFrom (x:xs) i = suffixFrom xs (i - 1)
 allSuffix xs = map (suffixFrom xs) [0..c] 
   where 
     c = length xs - 1
-   
+
 -- adds the index to each element in the array  i.e. the new element
 -- is of the form (element, index)
-makeTupel [] c = []
-makeTupel [x] c = [(x, c)]
-makeTupel (x:xs) c = (x,c): makeTupel xs (c+1)
+makeTupel [] i = []
+makeTupel [x] i = [(x, i)]
+makeTupel (x:xs) i = (x,i): makeTupel xs (i+1)
 
---sorts arrays which have tuple as ther elements.
--- the first element of the tuple should be an array/string again
-sortTupleArrays xs = mergeSort tupleComparrisor xs
+-- sorts arrays which have tuple as ther elements.
+-- the first element of the tuple should be an array/string
+sortTupleArrays xs = mergeSort tupleArrayComparritor xs
 
 -- create array of tuple of form (suffix, at index i)
 suffixTuple xs = makeTupel (allSuffix xs) 0
@@ -25,44 +27,9 @@ suffixTuple xs = makeTupel (allSuffix xs) 0
 -- crates the suffix sorted where the suffix are indexed
 sortedSuffix xs = sortTupleArrays (suffixTuple xs)
 
---creates suffix array
+--creates suffix array by selecting the index of the sorted suffix
 suffixArray xs = pickIndex (sortedSuffix xs)
   where 
     pickIndex [] = []
     pickIndex ((x,c):xs) = c: pickIndex xs
 
-
--- just sorting stuff
-
--- merge operation with adjustable comparrsion function
-merge comp [] ys = ys
-merge comp xs [] = xs
-merge comp (x:xs) (y:ys)
-  | comp x y = x : merge comp xs (y:ys)
-  | otherwise = y : merge comp (x:xs) ys
-  
--- merge sort
-mergeSort comp [] = []
-mergeSort comp [x] = [x]
-mergeSort comp xs = merge comp (mergeSort comp (firstN half (xs))) (mergeSort comp (fromN half xs))
-  where 
-    half = div (length xs) 2
-    
--- first n elements of an array
-firstN 0 xs = []
-firstN n (x:xs) = x : firstN (n-1) xs
-
--- last n elements of an array
-fromN 0 xs = xs
-fromN n (x:xs) = fromN (n-1) xs
-
--- compares two arrays
-arrayComparrisor [] ys = True
-arrayComparrisor xs [] = False
-arrayComparrisor (x:xs) (y:ys) 
-    | x < y = True
-    | x > y = False
-    | otherwise = arrayComparrisor xs ys
-    
--- compares two tuples which have arrays as their first elements
-tupleComparrisor (x,c) (y,i) = arrayComparrisor x y
